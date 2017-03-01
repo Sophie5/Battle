62f20f9eb8ba5a6018be1b350c62576c52950ae7
Flora Harvey <fharvey555@gmail.com>
2017-03-01 16:50:34 +0000

    Use sessions

--- app.rb
+++ app.rb
@@ -2,13 +2,21 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @Player1 = params[:Player1]
-    @Player2 = params[:Player2]
+    session[:Player1] = params[:Player1]
+    session[:Player2] = params[:Player2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @Player1 = session[:Player1]
+    @Player2 = session[:Player2]
     erb :play
   end
 end
