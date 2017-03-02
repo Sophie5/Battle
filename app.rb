12f02f4a5cf7596c57730fedb565bc341c29eb4b
Clem Capel-Bird <clemcapelbird@gmail.com>
2017-03-02 15:51:07 +0000

    Added an attack button and passed all tests

--- app.rb
+++ app.rb
@@ -19,4 +19,12 @@ class Battle < Sinatra::Base
     @player2 = session[:player2]
     erb :play
   end
+
+
+  get '/attack' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
+    erb :attack
+  end
+
 end
---
+++ spec/features/attack_spec.rb
@@ -0,0 +1,8 @@
+describe "attack" do
+it "confirms an attack has been successful" do
+  visit('/')
+  sign_in_and_play
+  click_button('Attack')
+  expect(page).to have_content('John attacked David')
+  end
+end
