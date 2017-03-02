3f264fb8e50c801318c9b1d7f91e42719ecd9b03
Sophie <sophie.k.mclean@btinternet.com>
2017-03-02 17:41:24 +0000

    extracting logic to the model

--- app.rb
+++ app.rb
@@ -1,30 +1,30 @@
 require 'sinatra/base'
-require 'player_class'
+require_relative 'lib/player_class'
 
 class Battle < Sinatra::Base
 
-  enable :sessions
+
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-   $player1 = Player.new(params[:player1])
-   $player2 = Player.new(params[:player2])
+   $player1 = Player.new(params[:player1_name])
+   $player2 = Player.new(params[:player2_name])
     redirect '/play'
   end
 
   get '/play' do
-    @player1 = $player1.name
-    @player2 = $player2.name
+    @player1_name = $player1.name
+    @player2_name = $player2.name
     erb :play
   end
 
 
   get '/attack' do
-    @player1 = $player1.name
-    @player2 = $player2.name
+    @player1_name = $player1.name
+    @player2_name = $player2.name
     erb :attack
   end
 
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,5 +1,5 @@
 def sign_in_and_play
-  fill_in('player1', with: 'John')
-  fill_in('player2', with: 'David')
+  fill_in('player1_name', with: 'John')
+  fill_in('player2_name', with: 'David')
   click_button('Submit')
 end
